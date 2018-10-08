c94359d095a86d91dd4a00a4d8240bf741009163
Jhoydev <jhoseph.dev@gmail.com>
2018-10-08 15:23:32 +0200

    MIGRATE DATES

--- resources/views/portal/event/index.blade.php
+++ resources/views/portal/event/index.blade.php
@@ -10,25 +10,37 @@
                     <h1 class="text-center"><i class="icon-calendar"></i> {{ $event->title }}</h1>
                     <hr>
                     <div class="row">
-                        <div class="col-12">
-                            <button class="btn btn-block btn-primary"><i class="fa fa-download" aria-hidden="true"></i> Descargar Memoria</button>
-                            <button class="btn btn-block btn-primary"><i class="fa fa-download" aria-hidden="true"></i> Descargar Certificado</button>
+                        <div class="col-auto mb-2">
+                            <button class="btn btn-primary rounded"><i class="fa fa-download" aria-hidden="true"></i> Descargar Memoria</button>
+                            <button class="btn btn-primary rounded"><i class="fa fa-download" aria-hidden="true"></i> Descargar Certificado</button>
                         </div>
-                        <div class="col-12">
-                            Tiquetes {{ count($details) }} <a href="{{ url("portal/customer/event/$event->id/details") }}">Ver</a>
+                        <div class="col-auto mb-2">
+                            <a class="btn btn-primary rounded" href="{{ url("portal/customer/event/$event->id/details") }}">
+                                Tiquetes
+                                <span class="badge badge-light"  data-toggle="tooltip" data-placement="top" title="Tus tiquetes">
+                                    {{ count($details) }}
+                                </span>
+                            </a>
                         </div>
                         @if (count($orders))
-                        <div class="col-12">
-                            <a href="{{ url("portal/event/$event->id/orders") }}">Detalle de compra </a>
-                        </div>
-                        @endif
-                        @if($details_null)
-                        <div class="col-12">
-                            Tiquetes sin asignar {{ $details_null }}
+                        <div class="col-auto mb-2">
+                            <a class="btn btn-success rounded" href="{{ url("portal/event/$event->id/orders") }}">
+                                Detalle de compra
+                                @if($details_null)
+                                    <span class="badge badge-danger"  data-toggle="tooltip" data-placement="top" title="Tiquetes sin asignar">
+                                        {{ $details_null }}
+                                    </span>
+                                @endif
+                            </a>
                         </div>
                         @endif
                     </div>
-                    <hr>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
                     <div class="row">
                         <div class="col-12">
                             <input type="hidden" id="url-calendar" value="{{ url('api/calendar/customer/'.Auth::user()->id) }}">
@@ -44,10 +56,11 @@
 <script>
     $(document).ready(function () {
         $("#calendar").fullCalendar({
+            defaultView: 'listWeek',
             header: {
                 left: 'prev,next today',
                 center: 'title',
-                right: 'month'
+                right: ''
             },
             events: {
                 url: document.querySelector('#url-calendar').value,
