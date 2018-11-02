9fbf378e364c2cacca4184b7ec1575d999a514fb
frogidev <39741685+frogidev@users.noreply.github.com>
2018-11-01 19:49:06 -0500

    Order Edit

--- resources/views/events/orders.blade.php
+++ resources/views/events/orders.blade.php
@@ -1,43 +1,43 @@
 @extends('layouts.template.melody')
 @section('content')
-@include('layouts.menssage_success')
-@include('errors.validation')
-@push('sidebar')
-@include('events.sidebar')
-@endpush
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <p class="display-5 text-center">{{ $event->title }}</p>
-                <p class="display-3 font-weight-bold text-center"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ordenes</p>
-                <hr>
-                @if(count($tickets))
-                <div class="row mb-2">
-                    <div class="col-lg-12 text-right">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-outline-info rounded" data-toggle="modal"
-                                data-target="#modelImportar">
-                            <i class="fa fa-download" aria-hidden="true"></i> Importar
-                        </button>
+    @include('layouts.menssage_success')
+    @include('errors.validation')
+    @push('sidebar')
+        @include('events.sidebar')
+    @endpush
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="display-5 text-center">{{ $event->title }}</p>
+                    <p class="display-3 font-weight-bold text-center"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ordenes</p>
+                    <hr>
+                    @if(count($tickets))
+                    <div class="row mb-2">
+                        <div class="col-lg-12 text-right">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-outline-info rounded" data-toggle="modal"
+                                    data-target="#modelImportar">
+                                <i class="fa fa-download" aria-hidden="true"></i> Importar
+                            </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade text-left" id="modelImportar" tabindex="-1" role="dialog"
-                             aria-labelledby="modelTitleId" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        @include('events.import_form')
+                            <!-- Modal -->
+                            <div class="modal fade text-left" id="modelImportar" tabindex="-1" role="dialog"
+                                 aria-labelledby="modelTitleId" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            @include('events.import_form')
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                @endif
-                <input type="hidden" id="url_order_confirm" value="{{ url("api/event/$event->id/order") }}">
-                <table id="table_datatable" class="table fade table-hover">
-                    <thead class="bg-primary text-white">
+                    @endif
+                    <input type="hidden" id="url_order_confirm" value="{{ url("api/event/$event->id/order") }}">
+                    <table id="table_datatable" class="table fade table-hover">
+                        <thead class="bg-primary text-white">
                         <tr class="text-center">
                             <th>#</th>
                             <th>Nombre</th>
@@ -47,52 +47,50 @@
                             <th class="text-center">Estado</th>
                             <th>Acciones</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($event->orders as $order)
-                        <tr class="text-center">
-                            <td>{{ $order->id }}</td>
-                            <td>{{ $order->customer->full_name }}</td>
-                            <td>{{ $order->customer->email }}</td>
-                            <td class="text-right">${{ number_format($order->price,2) }}</td>
-                            <td class="text-center"><span class="text-info font-weight-bold">{{ ($order->transaction_id) ? 'En Linea' : 'Efectivo' }}</span></td>
-                            <td class="text-center td-status">
-                                <span class="{{get_status_color($order->order_status_id )}} font-weight-bold">{{ $order->order_status->name }}</span>
-                            </td>
-                            <td class="text-right">
-                                @if($order->order_status_id == 2)
-                                <button class="btn btn-warning text-white btn-sm rounded" onclick="confirmOrder({{ $order->id }})">
-                                    <i class="fa fa-clock-o" aria-hidden="true"></i>Confirmar Orden
-                                </button>
-                                @endif
-                                <a class="" href="{{ route('event.orders.details',['event' => $event->id,'order' => $order->id]) }}"><i class="fa fa-sign-in-alt" aria-hidden="true"></i> Detalles</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach($event->orders as $order)
+                            <tr class="text-center">
+                                <td>{{ $order->id }}</td>
+                                <td>{{ $order->customer->full_name }}</td>
+                                <td>{{ $order->customer->email }}</td>
+                                <td class="text-right">${{ number_format($order->price,2) }}</td>
+                                <td class="text-center"><span class="text-info font-weight-bold">{{ ($order->transaction_id) ? 'En Linea' : 'Efectivo' }}</span></td>
+                                <td class="text-center td-status">
+                                    <span class="{{get_status_color($order->order_status_id )}} font-weight-bold">{{ $order->order_status->name }}</span>
+                                </td>
+                                <td class="text-right">
+                                    @if($order->order_status_id == 2)
+                                    <a class="text-success" href="#" onclick="confirmOrder({{ $order->id }})"><i class="fa fa-check-double" aria-hidden="true"></i> Confirmar Orden</a>
+                                    @endif
+                                    <a class="" href="{{ route('event.orders.details',['event' => $event->id,'order' => $order->id]) }}"><i class="fa fa-sign-in-alt" aria-hidden="true"></i> Detalles</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @push('scripts')
-@include('layouts.js.datatable')
+    @include('layouts.js.datatable')
 <script>
     function confirmOrder(order){
-    let tr = $(this.event.target).closest('tr');
-    let url = $("#url_order_confirm").val();
-    url = url + "/" + order + "/confirm";
-    let data = {
-    '_token' : $('input[name="csrf-token"]').val(),
+        let tr = $(this.event.target).closest('tr');
+        let url = $("#url_order_confirm").val();
+        url = url + "/" + order + "/confirm";
+        let data = {
+            '_token' : $('input[name="csrf-token"]').val(),
             '_method' : 'PUT'
-    };
-    $.post(url, data).done(function (res){
-    if (res.status){
-    $(tr).find('.td-status').html(`<span class="badge badge-success">pago</span>`);
-    alert('Orden Confirmada');
-    }
-    });
+        };
+        $.post(url,data).done(function (res){
+            if (res.status){
+                $(tr).find('.td-status').html(`<span class="badge badge-success">pago</span>`);
+                alert('Orden Confirmada');
+            }
+        });
     }
 </script>
 @endpush
